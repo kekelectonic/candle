dfc6516437fa1fffc6e4db53c8492151e6f3287b
Mikhail <mchernorotov@mail.ru>
2020-04-06 19:30:31 +0300

    06.04.2020

---
+++ view/construct.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300&display=swap" rel="stylesheet">    
+    <link rel="stylesheet" href="../css/construct.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <title>Construct</title>
+</head>
+<body>
+    <?php
+    include "header.html"
+    ?>
+    <h1>Создайте свечу </h1>
+    
+<div class="container">    
+        <div class="content">
+           
+        <p> Выберите цвет </p>
+                <select class="nav" name="select_color"> 
+                        <option class="red" value="red" selected ></option> 
+                        <option class="orange" value="orange"></option>
+                        <option class="yellow" value="yellow"></option>
+                        <option class="green" value="green"></option>
+                        <option class="blue" value="blue"></option>
+                        <option class="darkblue" value="darkblue"></option>
+                        <option class="purple" value="purple"></option>
+                </select>
+        </div>
+
+        <div class="content">
+           
+            <p> Выберите запах </p>
+                    <select class="nav" name="select_smell"> 
+                            <option value="1">channel №5</option> 
+                            <option value="2">trash</option>
+                            <option value="3">garbage</option>
+                            <option value="4">dirty MONK socks</option>
+                            <option value="5">dead fish</option>
+                            <option value="5">gspd </option>
+                            <option value="6"> Pugacheva</option>
+                    </select>
+            </div>
+</div>
+
+</body>
+</html>
---
+++ view/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href ="header.html">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <title>Main</title>
+</head>
+<body>
+    <?php
+    include "header.html";
+    ?>
+
+    <div class="container">
+        <div class="pic"><img src="../img/candle.jpg" width="200px" height="200px" alt=""></div>
+        <div class="pic"><img src="../img/candle.jpg" width="200px" height="200px" alt=""></div>
+        <div class="pic"><img src="../img/candle.jpg" width="200px" height="200px" alt=""></div>
+        <div class="pic"><img src="../img/candle.jpg" width="200px" height="200px" alt=""></div>
+        <div class="pic"><img src="../img/candle.jpg" width="200px" height="200px" alt=""></div>
+        <div class="pic"><img src="../img/candle.jpg" width="200px" height="200px" alt=""></div>
+        <div class="pic"><img src="../img/candle.jpg" width="200px" height="200px" alt=""></div>
+        <div class="pic"><img src="../img/candle.jpg" width="200px" height="200px" alt=""></div>
+    </div>
+
+</body>
+</html>
